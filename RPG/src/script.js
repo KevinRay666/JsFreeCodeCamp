5d1b8d0f82da8acc8ffdc9373c7a9f8733367f68
KevinRay666 <kevinray946@gmail.com>
2024-02-11 11:42:07 -0700

    Se esta mejorando el combate

--- RPG/src/script.js
+++ RPG/src/script.js
@@ -186,9 +186,13 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks."
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."
 
-    //health -= monsters[fighting].level;
     health -= getMonsterAttackValue(monsters[fighting].level);
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    if(isMonsterHit()){
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+    } else {
+        text.innerText += " You miss."
+    }
+    
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
 
@@ -200,6 +204,10 @@ function attack() {
         } else {
             defeatMonster();
         }
+    }
+
+    if(Math.random() <= .1){
+
     }
 
 }
@@ -241,5 +249,9 @@ function winGame() {
 function getMonsterAttackValue(level){
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
     console.log(hit);
-    return hit;
+    return hit > 0 ? hit : 0 ;
+}
+
+function isMonsterHit(){
+    return Math.random() > .2 || health < 20;
 }
