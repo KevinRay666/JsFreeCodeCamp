376ab5d715b34b382705aacc20cc5a34b42ba540
kevinray666 <18170460@itculiacan.edu.mx>
2024-03-27 17:50:53 -0700

    Se termino el proyecto Todo App

--- Todo App/script.js
+++ Todo App/script.js
@@ -10,10 +10,11 @@ const titleInput = document.getElementById('title-input');
 const dateInput =  document.getElementById('date-input');
 const descriptionInput = document.getElementById('description-input');
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () =>{
+    addOrUpdateTaskBtn.innerText = "Add Task"
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
       id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
@@ -24,7 +25,12 @@ const addOrUpdateTask = () =>{
     
      if (dataArrIndex === -1) {
       taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
     }
+
+    localStorage.setItem("data", JSON.stringify(taskData));
+
     updateTaskContainer();
     reset();
 }
@@ -47,7 +53,28 @@ const updateTaskContainer = () =>{
 }
 
 const deleteTask = (buttonEl) => {
-    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+        );
+        buttonEl.parentElement.remove()
+        taskData.splice(dataArrIndex,1)
+        localStorage.setItem("data", JSON.stringify(taskData));
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
 }
 
 const reset = () => {
@@ -58,13 +85,21 @@ const reset = () => {
     currentTask = {};
 }
 
+if(taskData.length){
+    updateTaskContainer()
+}
+
 openTaskFormBtn.addEventListener("click",() =>
     taskForm.classList.toggle("hidden")
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value
-    if(formInputsContainValues){
+    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+    (titleInput.value != currentTask.title) || 
+    (dateInput.value != currentTask.date) || 
+    (descriptionInput.value != currentTask.description);
+    if(formInputsContainValues && formInputValuesUpdated){
         confirmCloseDialog.showModal();
     } else {
         reset();
@@ -84,3 +119,22 @@ taskForm.addEventListener("submit", (e) => {
 
     addOrUpdateTask()
   });
+
+  /*
+  const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+  ];
+
+  localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+  localStorage.clear();
+
+
+  const getTaskArr = localStorage.getItem("data")
+  console.log(getTaskArr);
+
+  const getTaskArrObj = JSON.parse(localStorage.getItem("data"))
+  console.log(getTaskArrObj)
+  */
